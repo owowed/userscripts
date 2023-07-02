832e6f397c0b82f598ef81cef60626de5646a2b9
owowed <island@owowed.moe>
2023-07-02 13:30:22 +0700

    feat: create new pixiv bulk download userscript

---
+++ src/Pixiv Downloader/pixiv.ts
@@ -0,0 +1,60 @@
+import { makeMutationObserver, waitForElement } from "@owowed/oxi";
+import { requireNonNull } from "@shared/util.ts";
+
+export class PageEvent {
+    root = new EventTarget;
+    artworks = new EventTarget;
+
+    #firstTime = true;
+    #pageState: Array<"root" | "artworks"> = [];
+
+    constructor () {
+        this.initAsync();
+    }
+
+    async initAsync() {
+        const charcoal = await waitForElement(".charcoal-token > div", { maxTries: Infinity }).then(requireNonNull);
+
+        await this.dispatchEvents();
+
+        makeMutationObserver({ target: charcoal, childList: true }, async () => {
+            await this.dispatchEvents();
+        });
+    }
+
+    async dispatchEvents() {
+        this.root.dispatchEvent(new Event("navigate"));
+
+        if (this.#firstTime) {
+            this.root.dispatchEvent(new Event("navigate-begin"));
+            this.#pageState.push("root");
+            this.#firstTime = false;
+        }
+
+        if (window.location.href.includes("/artworks/")) {
+            this.artworks.dispatchEvent(new Event("navigate-begin"));
+            this.artworks.dispatchEvent(new Event("navigate"));
+            
+            const illustFigCaption = await waitForElement("div:has(> figure):has(> figcaption)").then(requireNonNull);
+
+            const observer = makeMutationObserver({ target: illustFigCaption, childList: true, once: true }, () => {
+                this.artworks.dispatchEvent(new Event("navigate"));
+            });
+
+            this.root.addEventListener("navigate", () => {
+                observer.disconnect();
+            }, { once: true });
+
+            this.#pageState.push("artworks");
+        }
+        else {
+            if (this.#pageState.includes("artworks")) {
+                this.artworks.dispatchEvent(new Event("navigate-end"));
+                this.#pageState.splice(this.#pageState.indexOf("artworks"), 1);
+            }
+        }
+    }
+}
+
+
+
