6c6917f460533262f9f3d2367230bf38d6d1fc77
owowed <island@owowed.moe>
2023-07-07 11:37:10 +0700

    refactor: seperate `PageEvent` into two classes
    there is `PageEvent` and `ArtworksEvent`, `PageEvent` behavior is same as
    the previous `PageEvent.root`, and `ArtworksEvent` is equivalent to the
    previous `PageEvent.artworks`
    
    `ArtworksEvent` is used for detecting page event on artworks page

---
+++ src/Pixiv Downloader/page.ts
@@ -0,0 +1,64 @@
+import { makeMutationObserver, waitForElement } from "@owowed/oxi";
+import { requireNonNull } from "@shared/util.ts";
+
+export class PageEvent extends EventTarget {
+    active = false;
+
+    constructor () {
+        super();
+        this.initAsync();
+    }
+
+    async initAsync() {
+        const charcoal = await waitForElement(".charcoal-token > div", { maxTries: Infinity }).then(requireNonNull);
+
+        if (!this.active) {
+            this.dispatchEvent(new Event("navigate-begin"));
+            this.dispatchEvent(new Event("navigate"));
+            this.active = true;
+        }
+
+        makeMutationObserver({ target: charcoal, childList: true }, async () => {    
+            if (!this.active) {
+                this.dispatchEvent(new Event("navigate-begin"));
+                this.active = true;
+            }
+
+            this.dispatchEvent(new Event("navigate"));
+        });
+    }
+}
+
+export class ArtworksEvent extends EventTarget {
+    #pageEvent = new PageEvent;
+    active = false;
+
+    constructor () {
+        super();
+        this.initAsync();
+    }
+
+    async initAsync() {
+        this.#pageEvent.addEventListener("navigate", async () => {
+            if (!window.location.pathname.includes("/artworks/")) return;
+    
+            this.dispatchEvent(new Event("navigate-begin"));
+            this.dispatchEvent(new Event("navigate"));
+    
+            const illustDesc = await waitForElement("div:has(> figure):has(> figcaption)").then(requireNonNull);
+    
+            const observer = makeMutationObserver({ target: illustDesc, childList: true }, () => {
+                this.dispatchEvent(new Event("navigate"));
+            });
+    
+            this.#pageEvent.addEventListener("navigate", () => {
+                this.dispatchEvent(new Event("navigate-end"));
+                this.active = false;
+                observer.disconnect();
+            }, { once: true });
+    
+            this.active = true;
+        });
+    }
+}
+
